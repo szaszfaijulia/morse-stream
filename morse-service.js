7a789fdef9977a402d68d9cdf861dd4c1bcb25fb
szaszfaijulia <szaszfai.julia@gmail.com>
2020-12-01 17:42:58 +0000

    comments added

--- morse-service.js
+++ morse-service.js
@@ -3,7 +3,7 @@ const os = require('os');
 
 const { Transform } = require('stream');
 
-const morseMap = {
+const morseMap = {  //object, összerendeli a morze jeleket a karakterekkel
     'A': '.-',
     'B': '-...',
     'C': '-.-.',
@@ -58,28 +58,28 @@ const separator = "#";
 const toMorse = new Transform({
   transform(chunk, encoding, callback){ //a transform metódusban alakítjuk át a chunk-ot
     this.push(chunk //push metódussal átadjuk a transform stream-nek az átalakított adatokat
-      .toString()
-      .split('')
-      .map(i => morseMap[i])
-      .join('#'));  //morze jelek elválasztása # karakterrel
+      .toString() //sztringgé alakítja a chunk-ot
+      .split('')  //szétválasztja a sztringet, tömbként adja vissza
+      .map(i => morseMap[i])  //a paraméterül kapott transzformáló függvényt alkalmazza a tömb minden elemére, a transzformált elemek tömbjével tér vissza
+      .join('#'));  //a tömb elemeinek (morze jelek) összekapcsolása # karakterrel
     callback(); //callback-el jelezzük a transform stream-nek, hogy befejeztük a chunk feldolgozását
   }
 })
 
 const fromMorse = new Transform({
-  transform(chunk, encoding, callback) {
-    this.push(chunk
-      .toString()
-      .split('#')
-      .map(morse => Object
-        .keys(morseMap)
-        .find(i => morseMap[i] === morse))
-      .join(''));
-  callback();
+  transform(chunk, encoding, callback) {  //chunk átalakítása
+    this.push(chunk //átadjuk a transform stream-nek az átalakított adatokat
+      .toString() //sztringé alakítja a chunk-ot
+      .split('#') //tömbként adja vissza a szétválasztott sztringet
+      .map(morse => Object  //transzformált elemek tömbjével tér vissza
+        .keys(morseMap) //morseMap objectben lévők neveit adja vissza
+        .find(i => morseMap[i] === morse))  //megkeresi az adott morze jelet a morseMap-ben
+      .join('')); //összekapcsolja a tömb elemeit
+  callback(); //chunk felfolgozásának befejezése
   }
 })
 
 module.exports = {
-    toMorse,
-    fromMorse
+    toMorse,  //readable stream-et kap, a karaktereket átalakítja morze jelekké
+    fromMorse //readable stream-et kap, a morze jeleket visszaalakítja betűkké
 }
